493d212ea681154bb05fe97ea08b540b8c86046d
Pier Valenzuela <50628232+r3ip@users.noreply.github.com>
2023-07-12 21:59:18 +0000

    primeros cambios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+const template = require('./template/template');
+const puppeteer = require('puppeteer-core');
+
 const toPdf = () => {
     
 }
--- src/template/template.js
+++ src/template/template.js
@@ -60,3 +60,5 @@ const styleContent = `
     text-shadow: 1px 1px 2px red,0 0 1em blue, 0 0 0.2em blue;
 }
 `
+
+export default { templateContent, styleContent}
